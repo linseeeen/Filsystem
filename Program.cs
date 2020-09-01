d7461a09de6d0e7809dff7c541eee70f8781677f
Linnea <linnea@klangby.com>
2020-09-01 10:51:20 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+
+namespace Filsystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //File.WriteAllText("words.txt", "Thing");
+            // string s = File.ReadAllText("words.txt");
+            if(File.Exists("words.txt"))
+            {
+            string[] words = File.ReadAllLines("words.txt");
+            System.Console.WriteLine(words[0]);
+
+            }
+            // Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
